036972f24c9e67c87017f98ced06697b1b60c865
thefishnews <609727@eriesd.org>
2026-06-23 17:14:56 -0400

    Update main plugin class with command registration

--- src/main/java/com/interactive/InteractiveChat.java
+++ src/main/java/com/interactive/InteractiveChat.java
@@ -1,24 +1,46 @@
 package com.interactive;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import com.interactive.commands.ChatCommand;
+import com.interactive.commands.ItemCommand;
+import com.interactive.commands.InventoryCommand;
+import com.interactive.commands.EnderchestCommand;
 import com.interactive.listeners.ChatListener;
-import com.interactive.listeners.ClickListener;
-import com.interactive.listeners.InventoryListener;
+import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * InteractiveChat - A Minecraft plugin for interactive chat with item/inventory/enderchest viewing
+ */
 public class InteractiveChat extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getLogger().info("§b[InteractiveChat] §fPlugin enabled!");
-        
+        // Log plugin startup
+        getLogger().info("================================================");
+        getLogger().info("InteractiveChat Plugin v" + getDescription().getVersion() + " is loading...");
+        getLogger().info("================================================");
+
         // Register listeners
         getServer().getPluginManager().registerEvents(new ChatListener(this), this);
-        getServer().getPluginManager().registerEvents(new ClickListener(this), this);
-        getServer().getPluginManager().registerEvents(new InventoryListener(this), this);
+        getLogger().info("✓ Chat listener registered");
+
+        // Register commands
+        getCommand("ichat").setExecutor(new ChatCommand(this));
+        getCommand("item").setExecutor(new ItemCommand(this));
+        getCommand("inv").setExecutor(new InventoryCommand(this));
+        getCommand("ec").setExecutor(new EnderchestCommand(this));
+        getLogger().info("✓ Commands registered: /ichat, /item, /inv, /ec");
+
+        // Load configuration
+        saveDefaultConfig();
+        getLogger().info("✓ Configuration loaded");
+
+        getLogger().info("================================================");
+        getLogger().info("InteractiveChat Plugin successfully enabled!");
+        getLogger().info("================================================");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("§b[InteractiveChat] §fPlugin disabled!");
+        getLogger().info("InteractiveChat Plugin has been disabled.");
     }
 }
